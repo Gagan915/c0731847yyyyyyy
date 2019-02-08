5be9394f943ff1851daf396773125f286bb2f75f
visualstudio <visualstudio@122-PC04>
2019-02-08 17:59:20 -0500

    5.51

--- c0731847/Program.cs
+++ c0731847/Program.cs
@@ -9,7 +9,16 @@ namespace c0731847
     class Program
     {
         static void Main(string[] args)
+
         {
+            class Node
+        {
+            public Node() { }
+            Node linkA;
+            Node elevatorUp;
+            String FloorNumber;
+        }
+        class elevator { }
         }
     }
 }

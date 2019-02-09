b7b06ce450034af1f68edfde86678a70d9f8f732
visualstudio <visualstudio@122-PC04>
2019-02-08 19:03:13 -0500

    7.00pm

--- c0731847/Program.cs
+++ c0731847/Program.cs
@@ -26,14 +26,25 @@ namespace c0731847
             Node SecondFloor;
             Node ThirdFloor;
             Node FourthFloor;
+           Node MainFloor;
 
             public void run()
             {
                 FirstFloor = new Node();
+                SecondFloor = new Node();
+                ThirdFloor = new Node();
+                FourthFloor = new Node();
                 FirstFloor.FloorNumber = "First Floor";
                 Console.WriteLine("floor number is {0}", FirstFloor.FloorNumber);
                 FirstFloor.elevatorUp = SecondFloor;
                 SecondFloor.FloorNumber = "Second Floor";
+                SecondFloor.elevatorUp = ThirdFloor;
+                ThirdFloor.FloorNumber = "Third Floor";
+                ThirdFloor.elevatorUp = FourthFloor;
+                FourthFloor.FloorNumber = "FourthFloor";
+                FourthFloor.elevatorUp = null;
+
+
                 Console.WriteLine("floor number is {0}", SecondFloor.FloorNumber);
             }
         }
